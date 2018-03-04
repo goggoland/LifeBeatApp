9e59ae0078c1ba80e4257cbe243f1041ec32b6ba
Gregory Albiani <gregoryalbiani@wc-dhcp226d129.eduroam.wireless.fsu.edu>
2018-03-04 00:33:37 -0500

    Initial commit

---
+++ app/src/main/java/com/example/gregoryalbiani/lifebeat/Permission2.java
@@ -0,0 +1,13 @@
+package com.example.gregoryalbiani.lifebeat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Permission2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permission4);
+    }
+}
---
+++ app/src/main/java/com/example/gregoryalbiani/lifebeat/live2.java
@@ -0,0 +1,13 @@
+package com.example.gregoryalbiani.lifebeat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class live2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_live2);
+    }
+}
